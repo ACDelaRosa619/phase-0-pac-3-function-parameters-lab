bbe4b2ed2961b0fe0ab7eb9e6be41dc34837fd11
Adrian <acdelarosa19@gmail.com>
2023-09-02 07:20:50 -0700

    completed labs

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+   if (name ==`Aki`)
+    return ("Hi, my name is Aki.")
+if (name ==`Samip`)
+return ("Hi, my name is Samip.")
+}
+
+function introductionWithLanguage(name, language) { 
+    if(name == `Aki`, language == `Ember.js`) {
+        return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    }
+    if(name == `Samip`, language == `React`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)}
+
+    function introductionWithLanguageOptional(name, language = `JavaScript`) { 
+        if(name = `Gracie`, language) 
+            return (`Hi, my name is ${name} and I am learning to program in ${language}.`)}
+
+            
